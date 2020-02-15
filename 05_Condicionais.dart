d2f143ef0bbd293a4d7a814b2a60c34d076220fa
Gustavo de Oliveira Ferreira <gust.oliveira.ferreira@gmail.com>
2020-02-14 23:54:06 -0300

    Adding 'switch - case - default'

--- 05_Condicionais.dart
+++ 05_Condicionais.dart
@@ -1,21 +1,18 @@
-main(){
-  if(1==1){
+main() {
+  if (1 == 1) {
     print("É obvio que é verdadeiro uai");
-  }
-  else{
+  } else {
     print("Não vai sai isso aqui huahauha");
   }
 
   var num1 = 5;
   var num2 = 10;
 
-  if(num1 > num2){
+  if (num1 > num2) {
     print("$num1 é maior que $num2");
-  }
-  else if(num1 < num2){
+  } else if (num1 < num2) {
     print("$num2 é maior que $num1");
-  }
-  else{
+  } else {
     print("$num1 é igual ao $num2");
   }
 
@@ -24,6 +21,28 @@ condition ? expr1 : expr2
 If condition is true, evaluates expr1 (and returns its value);
 otherwise, evaluates and returns the value of expr2.
 */
-  var teste = 1==2 ? "1 é igual a 2" : "1 é diferente de dois";
+  var teste = 1 == 2 ? "1 é igual a 2" : "1 é diferente de dois";
   print(teste);
+
+  String selecao = 'Brasil';
+
+  switch (selecao) {
+    case "Brasil":
+      print("5 Copas");
+      break;
+    case "Alemanha":
+      print("4 Copas");
+      break;
+    case "Argetina":
+      print("2 Copas");
+      break;
+    case "Portugal":
+      print("0 Copas");
+      break;
+    case "Espanha":
+      print("1 Copa");
+      break;
+    default:
+      print("Digite uma copa aí mermão...");
+  }
 }
